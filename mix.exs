b6e51870c66f5b6bf4649bc3e9c78ca2dbcbbb33
michele <michele.carri@gmail.com>
2018-04-30 15:29:27 +0200

    remove compilation warnings

--- mix.exs
+++ mix.exs
@@ -2,14 +2,16 @@ defmodule ExIban.Mixfile do
   use Mix.Project
 
   def project do
-    [app: :exiban,
-     version: "0.0.5",
-     elixir: "~> 1.2",
-     build_embedded: Mix.env == :prod,
-     start_permanent: Mix.env == :prod,
-     description: description,
-     package: package,
-     deps: deps]
+    [
+      app: :exiban,
+      version: "0.0.5",
+      elixir: "~> 1.2",
+      build_embedded: Mix.env() == :prod,
+      start_permanent: Mix.env() == :prod,
+      description: description(),
+      package: package(),
+      deps: deps()
+    ]
   end
 
   def application do
